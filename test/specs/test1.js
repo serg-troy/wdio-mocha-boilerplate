7cdd5b2714845124bcdef189786e7c2897c7f5fd
Sergey Troyeglazov <troyeglazov.s.a@gmail.com>
2020-03-15 00:15:37 -0400

    Template setup final

--- test/specs/test1.js
+++ test/specs/test1.js
@@ -1,10 +1,6 @@
 import { expect } from 'chai';
 
 describe('test', () => {
-  it('should describe', () => {
-    browser.url('www.google.com');
-  });
-
   it('should true be truthy', () => {
     expect(true).true;
   });
