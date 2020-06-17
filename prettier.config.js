4c32c032660db0bfb730aee5ce0035c9ae23cae8
Sergey Troyeglazov <troyeglazov.s.a@gmail.com>
2020-06-17 00:12:28 -0400

    Refactored June 2020

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: 'all',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    parser: 'babel',
+    printWidth: 100,
+};
+
+//npm install prettier --save-dev
